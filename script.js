dbcbe02759975f4a0d0ac91bd63767cccd1eebad
anglee <ang.lee@gmail.com>
2013-11-18 20:55:03 -0500

    add main module and controller

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+(function (angular) {
+    angular.module("myApp", []);
+    angular.controller("myCtrl", function ($scope) {
+        myCtrl.name = "AngularJS";
+    });
+
+})(angular);
